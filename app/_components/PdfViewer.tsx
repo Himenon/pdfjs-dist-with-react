f1e5090a2ac56eda1deab46635d98dc6491054ce
K.Himeno <6715229+Himenon@users.noreply.github.com>
2025-11-12 10:02:11 +0900

    chore: add code

---
+++ app/_components/PdfViewer.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { useEffect, useRef, useState } from "react";
+
+const loadPDF = async (canvas: HTMLCanvasElement, pdfPath: string) => {
+	// 動的インポートでブラウザ環境でのみpdf.jsを読み込む
+	const pdfjsLib = await import("pdfjs-dist");
+	// PDF.jsのworkerを設定
+	if (!pdfjsLib.GlobalWorkerOptions.workerSrc) {
+		// pdfjsLib.GlobalWorkerOptions.workerSrc = `//cdn.jsdelivr.net/npm/pdfjs-dist@${pdfjsLib.version}/build/pdf.worker.min.js`;
+		pdfjsLib.GlobalWorkerOptions.workerSrc = `https://unpkg.com/pdfjs-dist@5.4.394/build/pdf.worker.min.mjs`;
+	}
+
+	const loadingTask = pdfjsLib.getDocument(pdfPath);
+	const pdfDocument = await loadingTask.promise;
+	const pdfPage = await pdfDocument.getPage(1);
+	const viewport = pdfPage.getViewport({ scale: 1.0 });
+	canvas.width = viewport.width;
+	canvas.height = viewport.height;
+	const renderTask = pdfPage.render({
+		viewport,
+		canvas,
+	});
+	await renderTask.promise;
+};
+
+interface PdfViewerProps {
+	pdfUrl: string;
+}
+
+export default function PdfViewer({ pdfUrl }: PdfViewerProps) {
+	const [canvasRef, setCanvasRef] = useState<HTMLCanvasElement | null>(null);
+	const [loading, setLoading] = useState<boolean>(true);
+
+	// PDFをロード
+	useEffect(() => {
+		console.log({
+			canvasRef,
+		});
+		if (!canvasRef) {
+			return;
+		}
+		console.log("ここです");
+		loadPDF(canvasRef, pdfUrl)
+			.then(() => {
+				setLoading(false);
+			})
+			.catch((err) => {
+				console.error("Error loading PDF:", err);
+				setLoading(false);
+			});
+	}, [pdfUrl, canvasRef]);
+
+	return (
+		<div>
+			<canvas ref={setCanvasRef} width={"256px"} height="256px" />
+		</div>
+	);
+}
