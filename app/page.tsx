53eb9cb5084eb567e40d117e0df7692df2afb9d3
K.Himeno <6715229+Himenon@users.noreply.github.com>
2025-11-12 17:53:46 +0900

    fix: update site title  (#5)

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,20 @@ import { PDFViewerFrame } from "./_components/PDFViewerFrame";
 export default function Home() {
   return (
     <div>
-      <h1>PDF.js Viewer サンプル</h1>
+      <h1>PDF.js Viewer の実装サンプル</h1>
       <a href="./pdf-viewer" target="_blank" rel="noreferrer">
         PDF Viewer を新しいタブで開く
       </a>
+      <p>
+        Repository:{" "}
+        <a
+          href="https://github.com/Himenon/pdfjs-dist-with-react"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Himenon/pdfjs-dist-with-react
+        </a>
+      </p>
       <div style={{ width: "100vw", height: "80vh" }}>
         <PDFViewerFrame filename="sample.pdf" url="./sample.pdf" />
       </div>
